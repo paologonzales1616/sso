ff23692b4c1c502875b994b873886a5d255e6cf1
Paolo Gonzales <paologonzales1616@gmail.com>
2024-01-28 09:11:28 +0800

    SSO: Env setup

--- main.go
+++ main.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/django/v3"
+	"sso/utils"
 )
 
+func init() {
+	utils.ReadEnv()
+}
+
 func main() {
-	port := 3000
-	engine := django.New("./templates", ".html")
-
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
-		})
-	})
-
-	error := app.Listen(fmt.Sprintf(":%d", port))
-	if error != nil {
-		log.Fatal("Error occured while starting server at port:", port)
-	}
 }
