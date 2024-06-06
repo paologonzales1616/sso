8e504414f78f72994047cbac862210ff8ba46020
Paolo Gonzales <paologonzales1616@gmail.com>
2024-06-06 22:50:25 +0800

    SSO: health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
-app.get("/healthz", (req: Request, res: Response) => res.status(200));
+app.get("/healthz", (req, res) => res.status(200).send('OK'));
 
 export default app;
