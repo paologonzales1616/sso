3343f55a855818ffd3f627864e71589ceec9b01c
Paolo Gonzales <paologonzales1616@gmail.com>
2024-06-08 10:49:44 +0800

    SSO: setup ejs templating

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import path from 'path';
+
+import express, { Express } from "express";
 
 const app: Express = express();
 
-app.get("/healthz", (req, res) => res.status(200).send('OK'));
+app.set("views", path.join(__dirname + "/views"));
+app.set("view engine", "ejs");
+
+app.get("/healthz", (req, res) => res.status(200).send("OK"));
 
 export default app;
